154af9a52330a4d5496c7e85fb91fdecad393611
Alejandra Fabres <145504686+AAFabres@users.noreply.github.com>
2023-10-18 17:17:58 -0500

    Update HeyDay.sh
    making it pretty

--- HeyDay.sh
+++ HeyDay.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 
 #user is prompted to type name
-echo "Enter your name"
+echo "Welcome to Hey Day Celebration🍂! What's your name?"
 
 #read assigns user input to variable name
 read name 
 
 #print statement to user
-echo "Hey $name!"
+echo "Hey $name!, hope you have a wonderful day🤗!"
+exit
